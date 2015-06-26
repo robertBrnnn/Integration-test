aa50dc491a4b0a1814712f90ba33edfcfcaa742f
Administrator <robert.brnnn@gmail.com>
2015-06-26 15:21:53 +0100

    More uncovered code

--- app/src/main/java/example/HelloWorld.java
+++ app/src/main/java/example/HelloWorld.java
@@ -14,6 +14,11 @@ public class HelloWorld {
     }
 
     public void notCovered() {
+        
+        System.out.println("notCovered");
+        System.out.println("notCovered");
+        System.out.println("notCovered");
+        System.out.println("notCovered");
         System.out.println("notCovered");
     }
 	
